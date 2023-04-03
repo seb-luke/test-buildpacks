00475a82674b3fa318a70f61f653e41ec241c13c
--global <seb.luke@gmail.com>
2023-04-03 22:13:00 +0300

    Initial Spring Commit

---
+++ src/main/java/com/warptronics/testbuildpacks/TestBuildpacksApplication.java
@@ -0,0 +1,13 @@
+package com.warptronics.testbuildpacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuildpacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBuildpacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warptronics/testbuildpacks/TestBuildpacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warptronics.testbuildpacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBuildpacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
