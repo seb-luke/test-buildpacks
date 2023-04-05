f35f44b5df48539e01d8019df71fae6ad617d3d5
Sebastian Luca <seb.luke@gmail.com>
2023-04-05 12:24:58 +0300

    Demo CNV Commit

---
+++ cnb-demo/src/main/java/com/example/cnbdemo/CnbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnbDemoApplication.class, args);
+    }
+
+}
---
+++ cnb-demo/src/test/java/com/example/cnbdemo/CnbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
